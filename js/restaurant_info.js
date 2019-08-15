0eba8e2a8e9cba9fae6b162f4f52e7d91fad632d
Imran Khan <imran.khan@kpn.com>
2019-08-15 20:56:16 +0200

    refactored responsive designe and desktop view for restaurant details

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -23,7 +23,7 @@ initMap = () => {
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
         mapboxToken: 'pk.eyJ1IjoiaW1yYW5rMTUwNiIsImEiOiJjano1Y2ZtMXIwNHZxM2hrYWp1N2V0ZWFiIn0.ZKR5SNul5l1bmQxcRq7k8A',
-        maxZoom: 18,
+        maxZoom: 20,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
