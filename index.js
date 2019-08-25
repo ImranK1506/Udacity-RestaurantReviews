81cda6018778a53c0638518a119d50e9292412e6
Imran Khan <imran.khan@kpn.com>
2019-08-25 13:54:15 +0200

    created index.js for deployment

---
+++ index.js
@@ -0,0 +1,6 @@
+let http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello')
+}).listen(process.env.PORT);
