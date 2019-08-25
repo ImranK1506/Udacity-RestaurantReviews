c6d67b78f20c9d67d6313ca666c8195ce6ceb187
Imran Khan <imran.khan@kpn.com>
2019-08-25 14:32:43 +0200

    testing deploy

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let http = require('http');
+let https = require('https');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  // response.end('Hello')
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   // response.end('Hello')
+// }).listen(process.env.PORT);
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('Test');
 }).listen(process.env.PORT);
